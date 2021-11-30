910d375af05d95d1bc28dcac865b6444ad778bbb
Gonçalo Silva <goncalo.lslv.silva@gmail.com>
2021-11-30 12:01:28 +0000

    Exercise 4 completed, additional exercise 1 and 2 started (regarding script 6)

--- Mars/Files/TrabPrat6/Ex_aula6_4a.asm
+++ Mars/Files/TrabPrat6/Ex_aula6_4a.asm
@@ -5,7 +5,8 @@
 				# Mapa de registos:
 				# $a0: argc (endereço) -> fica no $t0
 				# $a1: argv[] (endereço da primeira) -> fica no $t1
-				#
+				# i: $t2
+				# Temp1: $t3
 				
 	.data
 	.eqv PRINT_STRING,4
@@ -16,7 +17,7 @@ str3:	.asciiz ": "
 	.text
 	.globl main
 main:	
-	move $t0,$a0		# $t0 = &argc
+	move $t0,$a0		# $t0 = argc
 	move $t1,$a1		# $t1 = &argv[]
 	
 	la $a0,str1
@@ -27,8 +28,28 @@ main:
 	li $v0,PRINT_INT10
 	syscall			# print_int10(argc);
 	
-	lw $a0,4($t1)		# $a0 = argc
+	li $t2,0			# i=0;
+for:	bge $t2,$t0,endf		# for(i=0; i < argc; i++) {
+	sll $t3,$t2,2		# $t3 = i << 2 (i*4)
+	addu $t3,$t1,$t3	# $t3 = &argv[0] + i = &argv[i]
+	
+	la $a0,str2
 	li $v0,PRINT_STRING
-	syscall
-
+	syscall			# print_string('\nP');
+	
+	move $a0,$t2
+	li $v0,PRINT_INT10	
+	syscall			# print_int10(i);
+	
+	la $a0,str3
+	li $v0,PRINT_STRING
+	syscall			# print_string(': ');
+	
+	lw $a0,0($t3)		# $a0 = argv[i]
+	li $v0,PRINT_STRING
+	syscall			# print_string(argv[i]);
+	
+	addiu $t2,$t2,1		# i++;
+	j for
+endf:
 	jr $ra 			# FIm do programa
---
+++ Mars/Files/TrabPrat6/Ex_aula6_xtra_2.asm
@@ -0,0 +1,11 @@
+	# Programa que lê e imprime a informação relativa aos 
+	# argumentos fornecidos no program arguments, info:
+		# o número de caracteres de cada um dos argumentos;
+		# o número de letras (maiúsculas e minúsculas) de cada um dos argumentos;
+		# a string com o maior número de caracteres.
+	# Método de acesso ao array por ponteiros
+	
+				# Mapa de registos:
+				#
+	.data
+	.eqv S
